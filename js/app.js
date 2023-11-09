02135a7c21b125774cddb1b50d70803e67a1c3d6
sikorskayaX <sikorskaya02@gmail.com>
2023-11-09 04:47:30 +0300

    Refactor CSS and HTML files for slider functionality
    - Refactored CSS file to add new styles for the slider
    - Added new font-face declarations for different font weights
    - Updated HTML file to include images for the slider
    - Updated JavaScript file to configure the slider options and breakpoints

--- js/app.js
+++ js/app.js
@@ -1,3 +1,18 @@
 ﻿const sliderMain = new Swiper('.slider_main', {
-    freeMode: true
+    freeMode: true,
+    centeredSledes: true,
+    parallax: true,
+    mousewheel:true,
+    breakpoints: {
+        0: {
+            slidesPerView: 2.5,
+            spaceBetween: 20
+        }, 
+        680:{
+            slidesPerView: 3.5,
+            spaceBetween: 60
+        }
+
+    }
+
 })
